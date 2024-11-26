17b962ceaea89e0951b1e3b583dc0e6738473f89
sanketspcc <102177322+sanketspcc@users.noreply.github.com>
2024-11-26 14:24:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,61 +15,59 @@ provider "azurerm" {
   subscription_id = "e6fdeeb6-2367-40d0-b980-ab5cb9be4885"
 }
 
-# Create a Resource Group if it doesn’t exist
-resource "azurerm_resource_group" "tfexample" {
-  name     = "my-terraform-rg"
+# Module: Resource Group
+module "resource_group" {
+  source  = "./modules/resource_group"
+  name    = "my-terraform-rg"
   location = "West Europe"
 }
 
-# Create a Virtual Network
-resource "azurerm_virtual_network" "tfexample" {
+# Module: Virtual Network
+module "virtual_network" {
+  source              = "./modules/virtual_network"
   name                = "my-terraform-vnet"
-  location            = azurerm_resource_group.tfexample.location
-  resource_group_name = azurerm_resource_group.tfexample.name
+  resource_group_name = module.resource_group.name
+  location            = module.resource_group.location
   address_space       = ["10.0.0.0/16"]
 }
 
-# Create a Subnet in the Virtual Network
-resource "azurerm_subnet" "tfexample" {
-  name                 = "my-terraform-subnet"
-  resource_group_name  = azurerm_resource_group.tfexample.name
-  virtual_network_name = azurerm_virtual_network.tfexample.name
+# Module: Subnet
+module "subnet" {
+  source              = "./modules/subnet"
+  name                = "my-terraform-subnet"
+  resource_group_name = module.resource_group.name
+  virtual_network_name = module.virtual_network.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# Create a Network Interface
-resource "azurerm_network_interface" "tfexample" {
+# Module: Network Interface
+module "network_interface" {
+  source              = "./modules/network_interface"
   name                = "my-terraform-nic"
-  location            = azurerm_resource_group.tfexample.location
-  resource_group_name = azurerm_resource_group.tfexample.name
-
-  ip_configuration {
-    name                          = "my-terraform-nic-ip-config"
-    subnet_id                     = azurerm_subnet.tfexample.id
-    private_ip_address_allocation = "Dynamic"
-  }
+  resource_group_name = module.resource_group.name
+  location            = module.resource_group.location
+  subnet_id           = module.subnet.id
 }
 
-# Create a Virtual Machine
-resource "azurerm_linux_virtual_machine" "tfexample" {
-  name                            = "my-terraform-vm"
-  location                        = azurerm_resource_group.tfexample.location
-  resource_group_name             = azurerm_resource_group.tfexample.name
-  network_interface_ids           = [azurerm_network_interface.tfexample.id]
-  size                            = "Standard_DS1_v2"
-  computer_name                   = "myvm"
-  admin_username                  = "azureuser"
-  admin_password                  = "Password1234!"
+# Module: Virtual Machine
+module "linux_virtual_machine" {
+  source                  = "./modules/linux_virtual_machine"
+  name                    = "my-terraform-vm"
+  location                = module.resource_group.location
+  resource_group_name     = module.resource_group.name
+  network_interface_ids   = [module.network_interface.id]
+  size                    = "Standard_DS1_v2"
+  computer_name           = "myvm"
+  admin_username          = "azureuser"
+  admin_password          = "Password1234!"  # NOTE: Use secure methods like Vault for sensitive values
   disable_password_authentication = false
-
-  source_image_reference {
+  source_image_reference = {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
     version   = "latest"
   }
-
-  os_disk {
+  os_disk = {
     name                 = "my-terraform-os-disk"
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
